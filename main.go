80041c4cd383bb1cf5010a1dbdcd2e7d0c9d2552
pong3ds <chaiyapong@3dsinteractive.com>
2018-09-14 11:08:58 +0700

    Add HAProxy for redis cluster

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
-import (
-	"time"
-
-	redis "github.com/3dsinteractive/redis-go-cluster"
-)
-
 func main() {
-	cluster, err := redis.NewCluster(
-		&redis.Options{
-			StartNodes:   []string{"127.0.0.1:7000", "127.0.0.1:7001", "127.0.0.1:7002"},
-			ConnTimeout:  50 * time.Millisecond,
-			ReadTimeout:  50 * time.Millisecond,
-			WriteTimeout: 50 * time.Millisecond,
-			KeepAlive:    16,
-			AliveTime:    60 * time.Second,
-		})
+	// cluster, err := redis.NewCluster(
+	// 	&redis.Options{
+	// 		StartNodes:   []string{"127.0.0.1:7000", "127.0.0.1:7001"},
+	// 		ConnTimeout:  50 * time.Millisecond,
+	// 		ReadTimeout:  50 * time.Millisecond,
+	// 		WriteTimeout: 50 * time.Millisecond,
+	// 		KeepAlive:    16,
+	// 		AliveTime:    60 * time.Second,
+	// 	})
 }
